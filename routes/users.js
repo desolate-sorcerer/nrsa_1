2e5d14e903b804710cf58a121573f7f580713826
stevekoromac <stevekoromac@gamil.com>
2025-04-02 21:33:55 +0200

    mongodb delete/get

--- routes/users.js
+++ routes/users.js
@@ -10,9 +10,13 @@ const router = express.Router();
 router.use(express.json());
 router.use(express.urlencoded({ extended: true }));
 
+//first page
+
 router.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../files/index.html'));
 })
+
+//get * users
 
 router.get('/users', async (req, res) => {
   try {
@@ -24,6 +28,14 @@ router.get('/users', async (req, res) => {
   }
 });
 
+//get user by id
+
+router.get('/:id', getUser, (req, res) => {
+  res.json(res.user);
+});
+
+//add user
+
 router.post('/add', async (req, res) => {
   const user = new userModel({
     name: req.body.username,
@@ -31,10 +43,38 @@ router.post('/add', async (req, res) => {
   });
   try {
     const newUser = await user.save();
-    res.status(201).json(user);
+    res.status(201).json(newUser);
   } catch (err) {
     res.status(400).json({ message: err.message });
   }
 });
+
+//delete user
+
+router.delete('/:id', getUser, async (req, res) => {
+  try {
+    await userModel.deleteOne(res.user);
+    res.status(201).json({ message: "user is removed" });
+  }
+  catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+})
+
+//middleware for getting users
+
+async function getUser(req, res, next) {
+  let user;
+  try {
+    user = await userModel.findById(req.params.id);
+    if (user == null) {
+      res.status(404).json({ message: "could not find user" });
+    }
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+  res.user = user;
+  next();
+}
 
 export default router;
